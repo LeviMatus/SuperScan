cf6e13ada0bfd5025103d7b371d6272f043fab4d
levi <thelevimatusproject@gmail.com>
2019-02-20 00:45:54 -0400

    string-scanner: add enum values

--- src/com/superscan/enums/Tokens.java
+++ src/com/superscan/enums/Tokens.java
@@ -1,7 +1,14 @@
 package com.superscan.enums;
 
 public enum Tokens {
-    INVALID,
-    INDETERMINATE,
-    STRING;
+    INVALID("INVALID"),
+    INDETERMINATE("INDETERMINATE"),
+    STRING("STRING");
+
+    private String name;
+
+    @Override
+    public String toString() {return name;}
+
+    Tokens(String name) {this.name = name;}
 }
