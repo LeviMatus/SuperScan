86daea085e4b207a0cdfa3249cb7c94023a65eef
levi <thelevimatusproject@gmail.com>
2019-02-19 22:13:41 -0400

    string-scanner: refactor scanner/interface into abstract class

--- src/com/superscan/scanners/ScannerInterface.java
+++ src/com/superscan/scanners/ScannerInterface.java
@@ -1,7 +1,15 @@
 package com.superscan.scanners;
 
 interface ScannerInterface {
-    StringScanner.Tokens transitionFunction(Character curr);
-    void resetStates(String token);
+
+    StringScanner.Tokens scan(String token, Integer start);
+
+    /**
+     * If an token is rejected, we need to know where it broke down.
+     * Read Ass3 Incorrect Input section for details on how to report this.
+     *
+     * @return Index where a token was rejected.
+     */
+    Integer errorLocation();
 
 }
