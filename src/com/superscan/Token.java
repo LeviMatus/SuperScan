522e5e26723b1ecb1397f7917a23c1ede4c2b4f9
levi <thelevimatusproject@gmail.com>
2019-02-22 15:36:29 -0400

    scanner-controller-mk2: add token class

---
+++ src/com/superscan/Token.java
@@ -0,0 +1,44 @@
+package com.superscan;
+
+import com.superscan.enums.Tokens;
+
+public class Token {
+
+    private String val;
+    private Integer row, col;
+    private Tokens type;
+
+    public Token(Integer row, Integer col) {
+        val = "";
+        this.row = row;
+        this.col = col;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    public void addChar(Character c) {
+        val += c;
+    }
+
+    public void setType(Tokens type) {
+        this.type = type;
+    }
+
+    public Tokens getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        if (type == null || type.equals(Tokens.INVALID) || type.equals(Tokens.INDETERMINATE))
+            return String.format("LEXICAL ERROR [%d:%d]: Invalid token `%s'",
+                    row, col, val);
+        return String.format("%s %d:%d", type.toString(), row, col);
+    }
+}
