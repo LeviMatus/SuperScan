90b59bcc847405d64ae08d696333bd514cbfefa8
levi <thelevimatusproject@gmail.com>
2019-02-23 23:13:57 -0400

    fun-refactor: implement State objects

---
+++ src/com/superscan/states/State.java
@@ -0,0 +1,25 @@
+package com.superscan.states;
+
+public interface State {
+
+    /**
+     * This State is an accepting state.
+     * @return true if accepted, false otherwise.
+     */
+    boolean isFinal();
+
+    /**
+     * Add a Transition to the calling State
+     * @param transition Transition object representing the transition from State A to State B
+     * @return
+     */
+    State addTransition(final Transition transition);
+
+    /**
+     * Given Character c, transition to a the next valid State.
+     * @param c Character which to read next.
+     * @return Accepted State.
+     * @throws IllegalStateException if c is invalid.
+     */
+    State transition(final Character c);
+}
---
+++ src/com/superscan/states/StateImpl.java
@@ -0,0 +1,40 @@
+package com.superscan.states;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StateImpl implements State {
+
+    private List<Transition> transitions;
+    private boolean isFinal;
+
+    /**
+     * Default constructor sets isFinal to false by default.
+     * Behaviour is changed by calling StateImpl(true).
+     */
+    public StateImpl() {
+        this(false);
+    }
+
+    public StateImpl(final boolean isFinal) {
+        this.transitions = new ArrayList<>();
+        this.isFinal = isFinal;
+    }
+
+    public boolean isFinal() {return this.isFinal;}
+
+    public State addTransition(Transition transition) {
+        this.transitions.add(transition);
+        return this;
+    }
+
+    public State transition(final Character c) {
+        return transitions
+                .stream()
+                .filter(transition -> transition.isValid(c))
+                .map(Transition::next)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("FAILURE"));
+    }
+
+}
