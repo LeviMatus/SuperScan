fdec49ff362841a39a8db94fb64d8110f96650d0
levi <thelevimatusproject@gmail.com>
2019-02-24 00:47:54 -0400

    fun-refactor: add logic for handling line number and indices

--- src/com/superscan/FSMImpl.java
+++ src/com/superscan/FSMImpl.java
@@ -1,16 +1,24 @@
 package com.superscan;
 
-import com.superscan.states.State;
+import com.superscan.enums.States;
+import com.superscan.states.StateImpl;
+import com.superscan.enums.Tokens;
+
+import java.util.List;
 
 public final class FSMImpl implements FSM {
 
-    private State current, initial;
+    private StateImpl current, initial;
     private Integer start, lineNum, offset;
+    private Token pendingToken;
+    private List<Token> acceptedTokens;
+    private boolean isAborting;
 
-    public FSMImpl(final State initial) {
+    public FSMImpl(final StateImpl initial) {
         this.start = this.lineNum = 1;
         this.offset = 0;
         this.current = this.initial = initial;
+        pendingToken = new Token(start, lineNum);
     }
 
     public void incrementOffset() { this.offset++; }
@@ -22,8 +30,42 @@ public final class FSMImpl implements FSM {
         this.offset = 1;
     }
 
+    private void handleWhitespace(Character c) {
+        if (c.equals('\n')) {
+            lineNum++;
+            start = 1;
+            offset = 1;
+        } else reset();
+    }
+
+    public void delimitToken() {
+        pendingToken.setType(current.getTokenType());
+        acceptedTokens.add(pendingToken);
+        current = initial;
+    }
+
+    private boolean isValidToken(Tokens token) {
+        return !token.equals(Tokens.INDETERMINATE) && !token.equals(Tokens.INVALID);
+    }
+
     public FSM transition(final Character c) {
-        return new FSMImpl(this.current.transition(c));
+        if (Character.isWhitespace(c)) {
+            if (this.isAborting) throw new IllegalStateException();
+
+            if (!current.equals(initial)) {
+                if (isValidToken(current.getTokenType())) {
+                    delimitToken();
+                    handleWhitespace(c);
+                    pendingToken = new Token(lineNum, start);
+                    return this;
+                }
+                return this;
+            }
+        }
+        pendingToken.addChar(c);
+        offset++;
+        this.current.transition(c);
+        return this;
     }
 
     public boolean canStop() {
