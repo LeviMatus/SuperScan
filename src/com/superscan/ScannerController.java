565d0ceba6a462dcfa9b1c300f285ff49e90b6e2
levi <thelevimatusproject@gmail.com>
2019-02-24 00:24:28 -0400

    fun-refactor: added state initialization

--- src/com/superscan/ScannerController.java
+++ src/com/superscan/ScannerController.java
@@ -2,6 +2,9 @@ package com.superscan;
 
 import com.superscan.enums.Tokens;
 import com.superscan.scanners.DFA;
+import com.superscan.states.State;
+import com.superscan.states.StateImpl;
+import com.superscan.transitions.TransitionImpl;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,6 +16,7 @@ import java.util.Arrays;
 public class ScannerController {
 
     private DFA dfa = new DFA();
+    private FSMImpl fsm;
     private ArrayList<Character> chars = new ArrayList<>();
     private Integer lineIndex;
 
@@ -28,6 +32,67 @@ public class ScannerController {
             System.exit(-1);
         }
         lineIndex = 1;
+
+        // States to belong to FSM
+        State S1 = new StateImpl();
+        State S2 = new StateImpl();
+        State S3 = new StateImpl();
+        State S4 = new StateImpl();
+        State S5 = new StateImpl(true, Tokens.NUMBER);
+        State S6 = new StateImpl();
+        State S7 = new StateImpl(true, Tokens.NUMBER);
+        State S8 = new StateImpl(true, Tokens.NUMBER);
+        State S9 = new StateImpl();
+        State S10 = new StateImpl(true, Tokens.NUMBER);
+        State S11 = new StateImpl(true, Tokens.NUMBER);
+        State S12 = new StateImpl(true, Tokens.NUMBER);
+        State S13 = new StateImpl();
+
+        // Initialize State Transitions
+        S1 = S1.addTransition(new TransitionImpl('0', S11));
+        S1 = S1.addTransition(new TransitionImpl('-', S9));
+        S1 = S1.addTransition(new TransitionImpl('+', S9));
+        S3 = S3.addTransition(new TransitionImpl('-', S6));
+        S3 = S3.addTransition(new TransitionImpl('+', S6));
+        S9 = S9.addTransition(new TransitionImpl('.', S13));
+        S11 = S11.addTransition(new TransitionImpl('b', S2));
+        S11 = S11.addTransition(new TransitionImpl('x', S4));
+        S11 = S11.addTransition(new TransitionImpl('.', S13));
+
+        for (int i = 0; i < 26; i++) {
+            if (i < 10) { // Numbered transitions
+                if (i == 0 || i == 1) { // binary number transitions;
+                    S2 = S2.addTransition(new TransitionImpl((char) i, S8));
+                    S8 = S8.addTransition(new TransitionImpl((char) i, S8));
+                }
+                if (i > 0) S1 = S1.addTransition(new TransitionImpl((char) i, S12));
+                S3 = S3.addTransition(new TransitionImpl((char) i, S7));
+                S4 = S4.addTransition(new TransitionImpl((char) i, S10));
+                S5 = S5.addTransition(new TransitionImpl((char) i, S5));
+                S6 = S6.addTransition(new TransitionImpl((char) i, S7));
+                S7 = S7.addTransition(new TransitionImpl((char) i, S7));
+                S9 = S9.addTransition(new TransitionImpl((char) i, S12));
+                S10 = S10.addTransition(new TransitionImpl((char) i, S10));
+                S11 = S11.addTransition(new TransitionImpl((char) i, S12));
+                S12 = S12.addTransition(new TransitionImpl((char) i, S12));
+                S13 = S13.addTransition(new TransitionImpl((char) i, S5));
+            }
+
+            if (Integer.parseInt("e") == ('a' + i)) {
+                S11 = addUpperAndLowercase(S11, S3, i);
+                S12 = addUpperAndLowercase(S12, S3, i);
+                S5 = addUpperAndLowercase(S5, S3, i);
+            }
+
+            S4 = addUpperAndLowercase(S4, S10, i);
+            S10 = addUpperAndLowercase(S10, S10, i);
+        }
+
+    }
+
+    private State addUpperAndLowercase(State state, State next, int i) {
+        state = state.addTransition(new TransitionImpl((char) ('a' + i), next));
+        return state.addTransition(new TransitionImpl((char) ('A' + i), next));
     }
 
     private void handleValidTokens() {
@@ -65,6 +130,7 @@ public class ScannerController {
             if (tokenType.equals(Tokens.INVALID))
                 if (handleInvalidToken()) return;
         }
+
         if (!tokenIsAccepted(tokenType)) handleInvalidToken();
         else handleValidTokens();
     }
