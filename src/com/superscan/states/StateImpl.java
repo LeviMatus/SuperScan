d16a0db90d1b6e363a54ad72a29a8ed1fb22e025
levi <thelevimatusproject@gmail.com>
2019-03-03 14:48:07 -0400

    fun-refactor: add fallback to begin halting scan on non-delimiter types

--- src/com/superscan/states/StateImpl.java
+++ src/com/superscan/states/StateImpl.java
@@ -1,9 +1,13 @@
 package com.superscan.states;
 
+import com.superscan.dfa.DFAImpl;
 import com.superscan.enums.Tokens;
 import com.superscan.transitions.TransitionImpl;
 
 public class StateImpl extends AbstractState {
+
+    private State fallback;
+    private String label;
 
     /**
      * Default constructor sets isFinal to false by default.
@@ -13,8 +17,12 @@ public class StateImpl extends AbstractState {
         this(false, Tokens.INDETERMINATE);
     }
 
-    public StateImpl(boolean stdDelimiter) {
-        this(false, Tokens.INDETERMINATE, stdDelimiter);
+    public StateImpl(boolean stdDelimiter, String label) {
+        this(false, Tokens.INDETERMINATE, stdDelimiter, label);
+    }
+
+    public StateImpl(boolean stdDelimiter, State fallback, String label) {
+        this(false, Tokens.INDETERMINATE, stdDelimiter, fallback, label);
     }
 
     /**
@@ -27,16 +35,38 @@ public class StateImpl extends AbstractState {
         this.addTransition(new TransitionImpl('\n', getInitialState()));
     }
 
+    public StateImpl(final boolean isFinal, final Tokens tokenType, String label) {
+        super(isFinal, tokenType);
+        this.label = label;
+        this.addTransition(new TransitionImpl(' ', getInitialState()));
+        this.addTransition(new TransitionImpl('\t', getInitialState()));
+        this.addTransition(new TransitionImpl('\n', getInitialState()));
+    }
+
     /**
      * Non-initial states transition back to the initial state on a whitespace character.
      */
-    public StateImpl(final boolean isFinal, final Tokens tokenType, final boolean stdDelimiter) {
+    public StateImpl(final boolean isFinal, final Tokens tokenType, final boolean stdDelimiter, String label) {
         super(isFinal, tokenType);
+        this.label = label;
         if (stdDelimiter) {
             this.addTransition(new TransitionImpl(' ', getInitialState()));
             this.addTransition(new TransitionImpl('\t', getInitialState()));
             this.addTransition(new TransitionImpl('\n', getInitialState()));
         }
+    }
+
+    public StateImpl(final boolean isFinal, final Tokens tokenType, final boolean stdDelimiter, State fallback, String label) {
+        this(isFinal, tokenType, stdDelimiter, label);
+        this.fallback = fallback;
+    }
+
+    @Override
+    public State attemptFallback(final DFAImpl dfa) {
+        if (this.fallback == null)
+            throw new IllegalArgumentException("FAILURE");
+        dfa.abort();
+        return this.fallback;
     }
 
 }
