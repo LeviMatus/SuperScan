31d881a930d06a41be834f08b48a875c15f0e221
levi <thelevimatusproject@gmail.com>
2019-03-03 13:16:24 -0400

    fun-refactor: add constructor to sates that do not delimit on whitespace

--- src/com/superscan/states/StateImpl.java
+++ src/com/superscan/states/StateImpl.java
@@ -13,15 +13,30 @@ public class StateImpl extends AbstractState {
         this(false, Tokens.INDETERMINATE);
     }
 
+    public StateImpl(boolean stdDelimiter) {
+        this(false, Tokens.INDETERMINATE, stdDelimiter);
+    }
+
     /**
      * Non-initial states transition back to the initial state on a whitespace character.
      */
     public StateImpl(final boolean isFinal, final Tokens tokenType) {
         super(isFinal, tokenType);
-
         this.addTransition(new TransitionImpl(' ', getInitialState()));
         this.addTransition(new TransitionImpl('\t', getInitialState()));
         this.addTransition(new TransitionImpl('\n', getInitialState()));
+    }
+
+    /**
+     * Non-initial states transition back to the initial state on a whitespace character.
+     */
+    public StateImpl(final boolean isFinal, final Tokens tokenType, final boolean stdDelimiter) {
+        super(isFinal, tokenType);
+        if (stdDelimiter) {
+            this.addTransition(new TransitionImpl(' ', getInitialState()));
+            this.addTransition(new TransitionImpl('\t', getInitialState()));
+            this.addTransition(new TransitionImpl('\n', getInitialState()));
+        }
     }
 
 }
