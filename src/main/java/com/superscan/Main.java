adefbe0877e3da2779232e52aba60d433e24a193
levi <thelevimatusproject@gmail.com>
2019-03-05 21:21:45 -0400

    cleanup: take filename from stdin

--- src/main/java/com/superscan/Main.java
+++ src/main/java/com/superscan/Main.java
@@ -3,7 +3,11 @@ package com.superscan;
 public class Main {
 
     public static void main(String[] args) {
-        ScannerController controller = new ScannerController("source.ss");
+        if (args.length == 0) {
+            System.out.println("Please provide the relative path of the file. Exiting.");
+            System.exit(-1);
+        }
+        ScannerController controller = new ScannerController(args[0]);
         controller.analyzeFile();
     }
 }
